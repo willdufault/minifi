f46c8b7c0d1398d1addd83e4e148bd96e775028f
wduf <wduf02@gmail.com>
2025-01-18 15:54:00 -0500

    removed car examples

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,42 +11,6 @@ function App() {
 
   const [articles, setArticles] = useState<Article[]>([])
 
-  async function getCars() {
-    const res = await fetch('/getCars', {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-    const json = await res.json()
-    console.log(json['body']['cars'])
-    alert('printed cars to console')
-  }
-
-  async function postCar() {
-    const res = await fetch('/createCar', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-    const json = await res.json()
-    console.log(json['body']['car'])
-    alert('printed new car to console')
-  }
-
-  async function deleteCars() {
-    const res = await fetch('/deleteCars', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-    const json = await res.json()
-    console.log(json['body']['message'])
-    alert('printed deleteCars output to console')
-  }
-
   async function getArticles() {
     const res = await fetch('/getArticles', {
       method: 'GET',
@@ -92,25 +56,19 @@ function App() {
   return (
     <>
       <div>
-        <button onClick={getCars}>get cars</button>
-        <button onClick={postCar}>post car</button>
-        <button onClick={deleteCars}>delete cars</button>
-      </div>
-      <br/>
-      <div>
         <button onClick={getArticles}>get articles</button>
         <button onClick={createArticle}>create article</button>
         <button onClick={deleteArticles}>delete articles</button>
       </div>
-      <br/>
+      <br />
       <h1>Articles:</h1>
       {articles.map((article, index) =>
-      <div key={index}>
-        <div style={{border: 'solid white 1px', padding: '1rem'}}>
-          <h1>{article.title}</h1>
-          <p>{article.body}</p>
+        <div key={index}>
+          <div style={{ border: 'solid white 1px', padding: '1rem' }}>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+          </div>
         </div>
-      </div> 
       )}
     </>
   )
